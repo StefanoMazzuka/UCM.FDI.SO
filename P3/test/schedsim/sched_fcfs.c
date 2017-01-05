739dc8f9fad830639a1ddba33aceab7f2d7d9bed
Kadaiser <usuarioso@debian>
2017-01-05 21:20:48 +0100

    Guia de la practica 3 añadida

---
+++ P3/test/schedsim/sched_fcfs.c
@@ -0,0 +1,44 @@
+#include "sched.h"
+
+static task_t* pick_next_task_fcfs(runqueue_t* rq,int cpu)
+{
+	task_t* t = head_slist(&rq->tasks);
+
+	/* Current is on the rq -> let's remove it */
+	if (t) 
+		remove_slist(&rq->tasks,t);
+
+	return t;
+}
+
+static void enqueue_task_fcfs(task_t* t,int cpu, int runnable)
+{
+	runqueue_t* rq = get_runqueue_cpu(cpu);
+
+	if (t->on_rq || is_idle_task(t))
+		return;
+
+	insert_slist(&rq->tasks,t); //Push task
+}
+
+static void task_tick_fcfs(runqueue_t* rq,int cpu)
+{
+	// FCFS does nothing
+}
+
+static task_t* steal_task_fcfs(runqueue_t* rq,int cpu)
+{
+	task_t* t = tail_slist(&rq->tasks);
+
+	if (t) 
+		remove_slist(&rq->tasks,t);
+
+	return t;
+}
+
+sched_class_t fcfs_sched= {
+	.pick_next_task=pick_next_task_fcfs,
+	.enqueue_task=enqueue_task_fcfs,
+	.task_tick=task_tick_fcfs,
+	.steal_task=steal_task_fcfs
+};
